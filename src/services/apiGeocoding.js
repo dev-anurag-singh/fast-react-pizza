eee75078205e9ba9913a79f101296d54e230a718
Anurag Singh <82097256+dev-anurag-singh@users.noreply.github.com>
2023-11-22 23:49:16 +0530

    Update apiGeocoding.js

--- src/services/apiGeocoding.js
+++ src/services/apiGeocoding.js
@@ -2,6 +2,7 @@ export async function getAddress({ latitude, longitude }) {
   const res = await fetch(
     `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}`
   );
+  // throw Error if res is not ok
   if (!res.ok) throw Error("Failed getting address");
 
   const data = await res.json();
